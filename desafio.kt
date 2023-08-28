65573cac93adc4493629ec261d35263a32c2f271
Leandro Mandu <leandro.om15@gmail.com>
2023-08-28 13:56:37 -0300

    implementação função matricular

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,36 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val name: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos)
+        inscritos.add(usuario)
+        println("${usuario.name} matriculado!")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //lista de conteudos
+    val contentList = listOf(ConteudoEducacional("Git", 40), ConteudoEducacional("Kotlin"), ConteudoEducacional("Linkedin", 25))
+    //instância do curso
+    val kotlinForm = Formacao("Kotlin", contentList)
+    //alunos
+    val user1 = Usuario(name="Aluno Um")
+    
+    //matriculando um aluno
+    kotlinForm.matricular(user1)
+    
+    //matriculando vários alunos
+    
+    
+    
+   // TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+   // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+   // aceitar matrícula de varios alunos ao mesmo tempo
 }
